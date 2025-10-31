9f7a6a6fed5689968014e46e4785ef0f74aba9fd
shalinibhavi525-sudo <shalinibhavi525@gmail.com>
2025-10-31 21:54:13 +0530

    Refactor capture_voice to accept audio bytes

--- modules/voice_listener.py
+++ modules/voice_listener.py
@@ -1,13 +1,16 @@
 import speech_recognition as sr
-def capture_voice():
- recognizer = sr.Recognizer()
- with sr.Microphone() as source:
- print("Listening...")
- audio = recognizer.listen(source, phrase_time_limit=5)
- try:
- text = recognizer.recognize_google(audio)
- return text
- except sr.UnknownValueError:
- return None
- except sr.RequestError:
- return None
+
+def capture_voice(audio_bytes):
+    if audio_bytes is None:
+        return None
+    
+    recognizer = sr.Recognizer()
+    audio_data = sr.AudioData(audio_bytes, 32000, 2)
+    
+    try:
+        text = recognizer.recognize_google(audio_data)
+        return text
+    except sr.UnknownValueError:
+        return None
+    except sr.RequestError:
+        return None
